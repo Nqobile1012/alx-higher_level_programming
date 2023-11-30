ee0745f28d4f42fa68a5d0826d7bf77b6fa91026
Nqobile1012 <buthelezinqobile45g@gmail.com>
2023-11-30 08:21:00 -0800

    Python

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,13 @@
+guillaume@ubuntu:~/0x02$ cat add_0.py
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
